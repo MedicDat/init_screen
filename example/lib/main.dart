5271e58d722b09a61864b2998d617fec32a57210
liquidiert <haberederkorbinian@googlemail.com>
2020-06-02 22:59:11 +0200

    feature init commit
      - added base class
      - added example project

---
+++ example/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:init_screen/init_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'init_screen demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return InitScreen([Text("test"), Text("test2")]);
+  }
+}
