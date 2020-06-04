73eeef75dc5380d4d7fbdb3ec01b0db2e0f1c878
liquidiert <haberederkorbinian@googlemail.com>
2020-06-04 16:03:20 +0200

    feature init release
      - implemented customization possibilities
      - added more examples

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,30 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:init_screen/init_screen.dart';
 
+import 'agb.dart';
+
 void main() {
-  runApp(MyApp());
+    runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'init_screen demo',
-      home: MyHomePage(),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'init_screen demo',
+            home: MyHomePage(),
+        );
+    }
 }
 
 class MyHomePage extends StatefulWidget {
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+    @override
+    _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  @override
-  Widget build(BuildContext context) {
-    return InitScreen([Text("test"), Text("test2")]);
-  }
+    final GlobalKey<InitScreenState> _key = GlobalKey();
+
+    _buildAGB() {
+        return SingleChildScrollView(
+            child: Container(
+                padding: EdgeInsets.all(20),
+                child: AGB(),
+            ),
+        );
+    }
+
+    _buildWithCustomNavs() {
+        return InitScreen(
+            [Text("test"), _buildAGB(), Text("[3]")],
+            key: _key, // needed for callback
+            customNavButtonBuilder: (index) { // custom NavButton builder
+                return IconButton(
+                    icon: Icon(Icons.ac_unit),
+                    onPressed: () => _key.currentState.navBtnClick(index),
+                );
+            },
+        );
+    }
+
+    _buildSimple() {
+        return InitScreen(
+            [Text("hi"), Text("am"), Text("simple")]
+        );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return _buildSimple();
+    }
 }
